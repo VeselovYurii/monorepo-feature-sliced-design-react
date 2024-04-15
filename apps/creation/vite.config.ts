b1d2bbf2aeb303abc081b2a1f059a297706f8e0d
y.veselov <navozru12345>
2024-04-15 09:25:13 -0400

    fix name space

--- apps/creation/vite.config.ts
+++ apps/creation/vite.config.ts
@@ -5,7 +5,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/ddd',
+  cacheDir: '../../node_modules/.vite/apps/creation',
 
   server: {
     port: 4200,
@@ -25,7 +25,7 @@ export default defineConfig({
   // },
 
   build: {
-    outDir: '../../dist/apps/ddd',
+    outDir: '../../dist/apps/creation',
     reportCompressedSize: true,
     commonjsOptions: {
       transformMixedEsModules: true,
@@ -42,7 +42,7 @@ export default defineConfig({
 
     reporters: ['default'],
     coverage: {
-      reportsDirectory: '../../coverage/apps/ddd',
+      reportsDirectory: '../../coverage/apps/creation',
       provider: 'v8',
     },
   },
