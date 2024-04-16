91b31fb64acc6a0408eca3f7762944526df7555d
y.veselov <navozru12345>
2024-04-16 10:17:10 -0400

    add shared for monorepo

--- apps/creation-e2e/src/e2e/app.cy.ts
+++ apps/creation-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('ddd-e2e', () => {
+describe('creation-e2e', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
