8b97539f31ec0a764b28805c3dc18b6759232cd0
y.veselov <navozru12345>
2024-04-16 10:53:23 -0400

    add tailwindcss

--- apps/creation/src/app/app.tsx
+++ apps/creation/src/app/app.tsx
@@ -1,11 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
+
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <div>
+    <div className='flex-1'>
       <NxWelcome title="creation" />
     </div>
   );
